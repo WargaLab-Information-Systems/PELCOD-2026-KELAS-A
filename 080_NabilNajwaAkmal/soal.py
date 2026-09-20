69b2af2784557c7a2c8f27bec54ffc9d8cdbb1da
nabilfp <nabilnajwaakmal02@gmail.com>
2026-09-20 09:49:54 +0700

    mengumpulkan hands on

---
+++ 080_NabilNajwaAkmal/soal.py
@@ -0,0 +1,26 @@
+# Studi Kasus : Berbelanja di Supermarket
+
+# Buat program yang bersifat dinamis dengan input :
+
+# Nama pembeli
+# Harga barang
+# Jumlah barang
+# Hitung total harga (harga barang x jumlah barang)
+
+# Diskon:
+# Jika total harga >= 100.000, maka akan mendapatkan diskon sebesar 10%
+# Jika total harga < 100.000, maka tidak akan mendapatkan diskon
+
+# Hitung nominal diskon:
+# Rumus:
+# Diskon (Rp)	= Harga Asli x Diskon (%)
+
+# Hitung total bayar:
+# Rumus:
+# Total Bayar	= Harga Asli - Diskon (Rp)
+
+# Pada hasil akhir, tampilkan:
+# Nama pembeli
+# Total harga
+# Diskon (jika tidak ada diskon, tampilkan 0%)
+# Total bayar (total harga setelah diskon)
